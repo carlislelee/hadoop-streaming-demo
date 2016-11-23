a644423fc1f14ad19b4465375aad8aad2b57098f
lizhaoxi <carlisle.lee.exe@gmail.com>
2016-11-23 22:26:56 +0800

    modify simple python wordcount

--- simple/simple-python/reducer.py
+++ simple/simple-python/reducer.py
@@ -2,17 +2,22 @@
 #-*- coding:utf-8 -*-
 import sys
 
-word_count = {}
+prev_word = None
+word_count = 0
 
 for line in sys.stdin:
   try:
     fields = line.strip().split('\t')
     word = fields[0]
-    word_count[word] = word_count.get(word, 0) + 1
-    
+    if word == prev_word:
+		word_count += 1
+	else:
+		if prev_word:
+			print '%s\t%s' % (prev_word, word_count)
+		word_count = 1
+		prev_word = word
   except:
     pass
 
-
-for word in word_count:
-  print '%s\t%s' % (word, word_count[word])
+if prev_word:
+	print '%s\t%s' % (prev_word, word_count)
